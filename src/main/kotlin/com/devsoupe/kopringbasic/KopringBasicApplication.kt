8795ba7c427d7283141fcc214664f6ce358c30ad
devsoupe <luvyum4e@gmail.com>
2023-09-05 11:24:55 +0900

    first commit

---
+++ src/main/kotlin/com/devsoupe/kopringbasic/KopringBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.devsoupe.kopringbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringBasicApplication>(*args)
+}
---
+++ src/test/kotlin/com/devsoupe/kopringbasic/KopringBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devsoupe.kopringbasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringBasicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
