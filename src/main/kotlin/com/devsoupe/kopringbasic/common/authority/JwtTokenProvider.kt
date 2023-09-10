0bb0a4c79f404ba6a809c46f3b46356e914b5af1
devsoupe <luvyum4e@gmail.com>
2023-09-10 22:46:43 +0900

    3.2. JwtToken 만들기 - getAuthentication, validateToken, getClaims

--- src/main/kotlin/com/devsoupe/kopringbasic/common/authority/JwtTokenProvider.kt
+++ src/main/kotlin/com/devsoupe/kopringbasic/common/authority/JwtTokenProvider.kt
@@ -1,12 +1,15 @@
 package com.devsoupe.kopringbasic.common.authority
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.*
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -25,8 +28,7 @@ class JwtTokenProvider {
    * Token 생성
    */
   fun createToken(authentication: Authentication): TokenInfo {
-    val authorities: String = authentication
-      .authorities
+    val authorities: String = authentication.authorities
       .joinToString(",", transform = GrantedAuthority::getAuthority)
 
     val now = Date()
@@ -43,4 +45,48 @@ class JwtTokenProvider {
 
     return TokenInfo("Bearer", accessToken)
   }
+
+  /**
+   * Token 정보 추출
+   */
+  fun getAuthentication(token: String): Authentication {
+    val claims: Claims = getClaims(token)
+
+    val auth = claims["auth"] ?: throw RuntimeException("잘못된 토큰입니다.")
+
+    // 권한 정보 추출
+    val authorities: Collection<GrantedAuthority> = (auth as String)
+      .split(",")
+      .map { SimpleGrantedAuthority(it) }
+
+    val principal: UserDetails = User(claims.subject, "", authorities)
+
+    return UsernamePasswordAuthenticationToken(principal, "", authorities)
+  }
+
+  /**
+   * Token */
+  fun validateToken(token: String): Boolean {
+    try {
+      getClaims(token)
+      return true
+    } catch (e: Exception) {
+      when (e) {
+        is SecurityException -> {} // Invalid JWT Token
+        is MalformedJwtException -> {} // Invalid JWT Token
+        is ExpiredJwtException -> {} // Expired JWT Token
+        is UnsupportedJwtException -> {} // Unsupported JWT Token
+        is IllegalArgumentException -> {} // JWT claims string is empty
+        else -> {}  // else
+      }
+      println(e.message)
+    }
+    return false
+  }
+
+  private fun getClaims(token: String): Claims = Jwts.parserBuilder()
+    .setSigningKey(key)
+    .build()
+    .parseClaimsJwt(token)
+    .body
 }
